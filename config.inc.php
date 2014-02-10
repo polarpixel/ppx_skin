288fb4f87a2f17cb3420d51f34a13cc720ea4f28
Peter Bickel <peter@polarpixel.de>
2014-02-10 13:55:03 +0100

    Body-class for theme
    Added CSS-class for body: AddOns can use this theme-specific class for styling.

--- config.inc.php
+++ config.inc.php
@@ -12,9 +12,16 @@ $REX['ADDON']['version'][$mypage] = '1.0';
 $REX['ADDON']['author'][$mypage] = 'Peter Bickel';
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
 
-if(!$REX['GG'])
-{
+if($REX['REDAXO']) {
+
+	$I18N->appendFile(dirname(__FILE__) .'/lang/');
+	function rex_be_style_ppx_skin_css_body($params) {
+		$params["subject"]["class"][] = "be-style-ppx-skin";
+		return $params["subject"];
+	}
+
 	require_once(dirname(__FILE__). '/extensions/extensions_ppx_replace.inc.php');
-	rex_register_extension('OUTPUT_FILTER', 'ppx_replace');
+	rex_register_extension('PAGE_BODY_ATTR', 'rex_be_style_ppx_skin_css_body');
+
 }
 ?>
